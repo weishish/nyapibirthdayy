aa30a14bff7e208a3dc9c726fc1f619d7ac8ce25
weishish <villotalorenz@gmail.com>
2026-07-18 16:32:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,592 @@
+// ===========================
+// MEMORY DATA
+// ===========================
+
+const memories = {
+
+memory1:{
+
+title:"Senior Highschool Moments",
+
+message:"This is the moment of us being a Senior Highschool Student. I still remember the days, where in I am still a shy person when I am around you. If I could just take back time, I will back at this moment and spend a lot of time with you.",
+
+media:[
+{
+    type:"image",
+    src:"1 (28).jpg"
+},
+{
+    type:"image",
+    src:"1 (109).jpg"
+},
+{
+    type:"image",
+    src:"1 (91).jpg"
+},
+{
+    type:"image",
+    src:"1 (54).jpg"
+},
+{
+    type:"video",
+    src:"AQMUPmDaxFnL5ffKWlfIf0NukRMIoDaYfe0FYvkyn2Pag20NOKODjwerW1wJT14-BBfm_E-DRTQg8Bx_CbFrFDslfvEsrgvwTQxOTeeCIA.mp4"
+},
+{
+    type:"image",
+    src:"1 (111).jpg"
+},
+{
+    type:"video",
+    src:"AQMN5N-sgIzydZ-gYTj30B2zbBAlF-dMVio9Rm_rKRt1RIg42Gwm0T64tzP8gjXGMrLVtYGXBS4iTl30fiE2tGL4M_13gMij-dV6WPzsLA.mp4"
+},
+{
+    type:"video",
+    src:"2 (4).mp4"
+},
+{
+    type:"image",
+    src:"1 (94).jpg"
+},
+{
+    type:"image",
+    src:"1 (78).jpg"
+}
+]
+
+},
+
+memory2:{
+
+title:"Our First Date",
+
+message:"April 22, 2025, The day where our first date at SM, I know this is not the first date of us, because we have a first date before of that and it happens on April 8, 2025, Do you still remember that? where in we build a Lego block together and spend a lot of time hihihi. Back on our first date, where in I still don't know where to start our date, sorry for that moment. Do you still remember nung nakita natin sila ate sa SM, HHAHHAHHAHHAHAHHAHHAHA Grabe, nag ka gulatan kami nun.",
+
+media:[
+{
+    type:"image",
+    src:"1 (34).jpg"
+},
+{
+    type:"video",
+    src:"2 (16).mp4"
+}
+]
+
+},
+
+memory3:{
+
+title:"Your Baby Pictures",
+
+message:"Look at this cutie pictures, ang cutie cutie hanuu",
+
+media:[
+{
+    type:"image",
+    src:"1 (1).jpg"
+},
+{
+    type:"image",
+    src:"1 (10).jpg"
+},
+{
+    type:"image",
+    src:"1 (13).jpg"
+},
+{
+    type:"image",
+    src:"1 (14).jpg"
+},
+{
+    type:"image",
+    src:"1 (31).jpg"
+},
+{
+    type:"image",
+    src:"1 (35).jpg"
+},
+{
+    type:"image",
+    src:"1 (37).jpg"
+},
+{
+    type:"image",
+    src:"1 (43).jpg"
+},
+{
+    type:"image",
+    src:"1 (44).jpg"
+},
+{
+    type:"image",
+    src:"1 (55).jpg"
+},
+{
+    type:"image",
+    src:"1 (59).jpg"
+},
+{
+    type:"image",
+    src:"1 (63).jpg"
+},
+{
+    type:"image",
+    src:"1 (68).jpg"
+},
+{
+    type:"image",
+    src:"1 (72).jpg"
+},
+{
+    type:"image",
+    src:"1 (75).jpg"
+},
+{
+    type:"image",
+    src:"1 (76).jpg"
+},
+{
+    type:"image",
+    src:"1 (79).jpg"
+},
+{
+    type:"image",
+    src:"1 (80).jpg"
+},
+{
+    type:"image",
+    src:"1 (92).jpg"
+},
+{
+    type:"image",
+    src:"1 (102).jpg"
+},
+{
+    type:"image",
+    src:"1 (106).jpg"
+},
+{
+    type:"image",
+    src:"1 (110).jpg"
+},
+{
+    type:"image",
+    src:"1 (114).jpg"
+}
+]
+
+},
+
+memory4:{
+
+title:"NYAPII BIRTHDAYYY",
+
+message:"Happiest Birthday to this beautiful, Hardworking and Pretty Girl, I hope your day becomes special, because you are such a special girl, I hope maging masaya ikaw sa iyong ka arawan at sana po magustuhan mo ang mga regalong ibibigay ko sa Iyo. Babbyyy, I loveee youuu soo muchhh, I will always be here for you and I will support you from a far, I know you had so much struggles and problems, but I know that you will be able to overcome all of those challenges, Gooood Luckkk to your journey babbyyy, once again Happpyyy Birthhdayyy, I loveee you so muchhh",
+
+media:[
+{
+    type:"image",
+    src:"1 (9).jpg"
+},
+{
+    type:"image",
+    src:"1 (45).jpg"
+},
+{
+    type:"image",
+    src:"1 (114).jpg"
+},
+{
+    type:"image",
+    src:"1 (70).jpg"
+}
+]
+
+},
+
+memory5:{
+
+title:"NAMNAMNAM Momets",
+
+message:"Every moments na mag kasabay tayong kumain it feels like different, because when I am with you the foods taste better, I don't know, but It feels like I am safe and comfortable when I am eating by your side. Alam mo yung feeling na parang gumagaan lahat at tumatahimik kapag kausap kita habang kumakain, I am the moat happiest person when I am with you, Thank you so much, I loveee youuuuu",
+
+media:[
+{
+    type:"image",
+    src:"1 (19).jpg"
+},
+{
+    type:"image",
+    src:"1 (39).jpg"
+},
+{
+    type:"image",
+    src:"1 (61).jpg"
+},
+{
+    type:"image",
+    src:"1 (63).jpg"
+},
+{
+    type:"image",
+    src:"1 (106).jpg"
+},
+{
+    type:"image",
+    src:"1 (118).jpg"
+}
+]
+
+},
+
+memory6:{
+
+title:"Our Dates",
+
+message:"Every dates we had tells a lot of different stories. Roblox is the first we had so much spend time with, do you still remember that feeling, we spend a lot of time hanggang madaling araw hihihi. All of our dates are very special, I still remember the day na gumala tayo nung college na tayo, yung araw na wala pa tayong masyadong pictures, and na alala ko na super lakas ng ulan nun, kaya nag antay muna tayo na tumila ang ulan. Next is yung nag ka sakit ako and you gave me the cravings I want when I am sick back on that day, Next is the second date natin sa SM and ni adopt natin si mcmack and our first ever photobooth. Next is the day that you're sad and wanted to cry kasi naiinis ka dahil hindi malaman kung sasama ba ang mga kaibigan mo kaya pinuntahan nalang kita at nag date tayu hihihi, sabi rin ni tita mag date nalang tayu hihi. Next is yung araw na nag exchange gift tayu hihihi, and yung araw na ni open natin yung message natin sa isa't isa nung April hihi. Next is the 11th day of the month where we go to Church together and we play our game which is XOX. Next is the Valentine's Day na hindi natin sigurado na ma cecelebrate natin, but I still wait for you hihihihi. Next is you know, hihihi, February 27, 2026 hihihihi, Next is yung nag chowking tayu and nahulog ko yung halu halu HAHAHAHAHHAHAHHA. Next is yung araw na sinamahan mo ako mag pagawa ng salamin and pinuntahan natin sila tita and nag punta tayu sa SM kasama siwaa, and that day is the very first flower that I've ever had. Next is the celebration of my Birthday where in My Birthday Becomes very important of that day. Next is yung gala natin bago mag pasukan uwit, unang ginawa natin nun is nag simba tayu and we go to SM after. Next is our recent date which we celebrate, because enod enod na ang exam mu. Babyyy, I hope we have a lot of memories in the future, I hope next is on something far na hihihihi",
+
+media:[
+{
+    type:"image",
+    src:"hi.jpg"
+},
+{
+    type:"image",
+    src:"4.jpg"
+},
+{
+    type:"image",
+    src:"1 (82).jpg"
+},
+{
+    type:"image",
+    src:"1 (97).jpg"
+},
+{
+    type:"image",
+    src:"1 (93).jpg"
+},
+{
+    type:"video",
+    src:"2 (14).mp4"
+},
+{
+    type:"image",
+    src:"1 (30).jpg"
+},
+{
+    type:"video",
+    src:"2 (10).mp4"
+},
+{
+    type:"image",
+    src:"1 (51).jpg"
+},
+{
+    type:"image",
+    src:"1 (62).jpg"
+},
+{
+    type:"image",
+    src:"1 (95).jpg"
+},
+{
+    type:"image",
+    src:"1 (8).jpg"
+},
+{
+    type:"image",
+    src:"1 (81).jpg"
+},
+{
+    type:"image",
+    src:"1 (5).jpg"
+},
+{
+    type:"image",
+    src:"1 (36).jpg"
+},
+{
+    type:"image",
+    src:"1 (18).jpg"
+},
+{
+    type:"image",
+    src:"1 (38).jpg"
+},
+{
+    type:"image",
+    src:"1 (89).jpg"
+},
+{
+    type:"video",
+    src:"2 (7).mp4"
+},
+{
+    type:"image",
+    src:"1 (60).jpg"
+},
+{
+    type:"image",
+    src:"1 (108).jpg"
+},
+{
+    type:"image",
+    src:"1 (20).jpg"
+},
+{
+    type:"image",
+    src:"1 (50).jpg"
+},
+{
+    type:"image",
+    src:"1 (69).jpg"
+},
+{
+    type:"image",
+    src:"1 (100).jpg"
+},
+]
+
+},
+memory7:{
+
+title:"KAPIBAYAAAA",
+
+message:"Capybara ang isa sa pinaka lovee loveee mo and kapibaya is our first ever baby",
+
+media:[
+{
+    type:"image",
+    src:"1 (7).jpg"
+},
+{
+    type:"image",
+    src:"1 (46).jpg"
+},
+{
+    type:"image",
+    src:"1 (32).jpg"
+},
+{
+    type:"image",
+    src:"1 (27).jpg"
+},
+{
+    type:"image",
+    src:"1 (93).jpg"
+}
+]
+
+},
+memory8:{
+
+title:"Our Photobooth",
+
+message:"Every photobooth we had is a little story of us, our smile, our laughter and the moments we never wanted to end hihi, hugg huggg",
+
+media:[
+{
+    type:"image",
+    src:"1 (25).jpg"
+},
+{
+    type:"image",
+    src:"poto.jpg"
+},
+{
+    type:"image",
+    src:"1 (29).jpg"
+},
+{
+    type:"image",
+    src:"1 (23).jpg"
+},
+{
+    type:"image",
+    src:"1 (3).jpg"
+},
+{
+    type:"image",
+    src:"hihi.jpg"
+},
+{
+    type:"image",
+    src:"1 (48).jpg"
+},
+{
+    type:"image",
+    src:"1 (57).jpg"
+}
+
+]
+
+}
+
+};
+
+// ===========================
+
+let currentMemory = "";
+let currentPhoto = 0;
+
+// ===========================
+
+function openMemory(name){
+
+currentMemory = name;
+
+currentPhoto = 0;
+
+document.getElementById("galleryPage").style.display="none";
+
+document.querySelector("header").style.display="none";
+
+document.getElementById("memoryPage").style.display="block";
+
+document.getElementById("memoryTitle").innerHTML=memories[name].title;
+
+document.getElementById("memoryMessage").innerHTML=memories[name].message;
+
+showPhoto();
+
+}
+
+// ===========================
+
+function showPhoto(){
+
+    const item = memories[currentMemory].media[currentPhoto];
+
+    const img = document.getElementById("slideImage");
+    const video = document.getElementById("slideVideo");
+    const source = document.getElementById("videoSource");
+
+    if(item.type === "image"){
+
+        video.pause();
+        video.style.display = "none";
+
+        img.style.display = "block";
+        img.src = item.src;
+
+    }else{
+
+        img.style.display = "none";
+
+        video.style.display = "block";
+
+        source.src = item.src;
+
+        video.load();
+        video.play();
+
+    }
+
+}
+
+// ===========================
+
+function nextPhoto(){
+
+if(currentPhoto <
+memories[currentMemory].media.length-1){
+
+currentPhoto++;
+
+}else{
+
+currentPhoto=0;
+
+}
+
+showPhoto();
+
+}
+
+// ===========================
+
+function previousPhoto(){
+
+if(currentPhoto>0){
+
+currentPhoto--;
+
+}else{
+
+currentPhoto=
+memories[currentMemory].media.length-1;
+
+}
+
+showPhoto();
+
+}
+
+// ===========================
+
+function closeMemory(){
+
+document.getElementById("memoryPage").style.display="none";
+
+document.getElementById("galleryPage").style.display="block";
+
+document.querySelector("header").style.display="block";
+
+}
+
+// ===========================
+// KEYBOARD SUPPORT
+// ===========================
+
+document.addEventListener("keydown",function(e){
+
+if(document.getElementById("memoryPage").style.display=="block"){
+
+if(e.key==="ArrowRight"){
+
+nextPhoto();
+
+}
+
+if(e.key==="ArrowLeft"){
+
+previousPhoto();
+
+}
+
+if(e.key==="Escape"){
+
+closeMemory();
+
+}
+
+}
+
+});
+
+// ===========================
+// MUSIC
+// ===========================
+
+const music=document.getElementById("bgMusic");
+
+const musicBtn=document.getElementById("musicBtn");
+
+musicBtn.addEventListener("click",function(){
+
+if(music.paused){
+
+music.play();
+
+musicBtn.innerHTML="🔊 Music On";
+
+}else{
+
+music.pause();
+
+musicBtn.innerHTML="🎵 Music";
+
+}
+
+});
